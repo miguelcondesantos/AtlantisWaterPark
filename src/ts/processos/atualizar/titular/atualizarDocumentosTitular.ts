d74b93a9d101a77db1fc3e894e3652ea593152de
Miguel Conde Santos <miguelconde121004@gmail.com>
2024-04-19 16:11:51 -0300

    arrumando erro de repetição em atualizarDocumentoTitular

--- src/ts/processos/atualizar/titular/atualizarDocumentosTitular.ts
+++ src/ts/processos/atualizar/titular/atualizarDocumentosTitular.ts
@@ -19,13 +19,13 @@ export default class AtualizarDocumentosTitular extends Processo {
 
         console.log(`Cpf`);
         const novoCPF = this.entrada.receberTexto('Qual o novo CPF?');
-        const dataExpedicaoCPF = this.entrada.receberData('Qual a nova data de expedição do CPF?\n');
+        const dataExpedicaoCPF = this.entrada.receberData('Qual a nova data de expedição do CPF?');
         const documentoCPF = new Documento(novoCPF, TipoDocumento.CPF, dataExpedicaoCPF);
         this.cliente.Documentos.push(documentoCPF);
 
         console.log(`Rg`);
         const novoRG = this.entrada.receberTexto('Qual o novo RG?');
-        const dataExpedicaoRG = this.entrada.receberData('Qual a nova data de expedição do RG?\n');
+        const dataExpedicaoRG = this.entrada.receberData('Qual a nova data de expedição do RG?');
         const documentoRG = new Documento(novoRG, TipoDocumento.RG, dataExpedicaoRG);
         this.cliente.Documentos.push(documentoRG);
 
